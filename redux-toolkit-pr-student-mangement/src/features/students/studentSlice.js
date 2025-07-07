a889691a996348aa5ee06aec6791a4e3fc955f02
yadavsuraj999 <yadavsuraj8425@gmail.com>
2025-07-07 17:01:21 +0530

    add handle change

---
+++ redux-toolkit-pr-student-mangement/src/features/students/studentSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const studentSlice = createSlice({
+    name: "students",
+    initialState: {
+        list: []
+    },
+    reducers: {
+        addstudent: () => {
+
+        },
+        deletestudent: () => {
+
+        },
+        editestudent: () => {
+
+        },
+    }
+})
