3da0f67a7e3e8d5222d6deb3c357541124494516
yadavsuraj999 <yadavsuraj8425@gmail.com>
2025-07-23 18:45:02 +0530

    add delete function

---
+++ axios-api/src/App.jsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+import { useState } from "react"
+
+const App = () => {
+    const [users, setUsers] = useState([])
+
+    const fetchuser = async ()=>{
+        const data = 
+    }
+
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
--- mock-api/src/App.jsx
+++ mock-api/src/App.jsx
@@ -1,33 +1,83 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const App = () => {
-    const [users, setUser] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [input, setInput] = useState({ name: "", age: "" });
 
-    const URL = "http://localhost:5500/users"
-    useEffect(()=>{
-        const featchuser = async ()=>{
-            const res = await fetch(URL)
-            const data = await res.json()
-            setUser(data)
-        }
-        featchuser()
-    }, [])
+    const URL = "http://localhost:5000/users";
 
+    const fetchUser = async () => {
+        const res = await fetch(URL);
+        const data = await res.json();
+        setUsers(data);
+    };
 
-  return (
-    <div>
-        {
-            users.map((user)=>{
-                return(
-                    <div>
-                        <h1>{user.name}</h1>
-                        <h3>age :- {user.age}</h3>
-                    </div>
-                )
-            })
-        }
-    </div>
-  )
-}
+    useEffect(() => {
+        fetchUser();
+    }, []);
 
-export default App
+    const handlechange = (e) => {
+        setInput({ ...input, [e.target.id]: e.target.value });
+    };
+
+    const handlesubmit = async (e) => {
+        e.preventDefault();
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(input)
+        });
+        setInput({ name: "", age: "" });
+       fetchUser();
+    };
+
+    const handledelete = async (id) => {
+        await fetch(`http://localhost:5000/users/${id}`, {
+            method: "DELETE"
+        });
+        fetchUser();
+    };
+
+    const handleedit = async (id) => {
+        
+        await fetch(`http://localhost:5000/users/${id}`, {
+            method: "PUT"
+        })
+        fetchUser()
+    }
+
+    return (
+        <div>
+            <form onSubmit={handlesubmit}>
+                <input
+                    id="name"
+                    type="text"
+                    placeholder="Enter Your Name"
+                    value={input.name}
+                    onChange={handlechange}
+                />
+                <input
+                    id="age"
+                    type="text"
+                    placeholder="Enter Your age"
+                    value={input.age}
+                    onChange={handlechange}
+                />
+                <button>submit</button>
+            </form>
+
+            {users.map((user) => (
+                <div key={user.id}>
+                    <h1>{user.name}</h1>
+                    <h3>age :- {user.age}</h3>
+                    <button onClick={() => handledelete(user.id)}>❌</button>
+                    <button onClick={()=> handleedit(user.id)}>✏️</button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default App;
