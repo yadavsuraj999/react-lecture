715995356231ac87005114a51f2a7a660f4ca8ff
yadavsuraj999 <yadavsuraj8425@gmail.com>
2025-08-11 16:49:59 +0530

    add delete function

--- Firebase-fire-store/src/App.jsx
+++ Firebase-fire-store/src/App.jsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
 import AddAnimal from "./components/AddAnimal";
 import AnimalList from "./components/Animallist";
 import Dashboard from "./components/Dashboard";
@@ -9,9 +9,8 @@ import { useEffect, useState } from "react";
 
 
 function App() {
-
-
   const [animals, setAnimal] = useState([])
+  const [animalId, setAnimalId] = useState(null)
   const [input, setInput] = useState({
     name: "", species: "", age: ""
   })
@@ -23,7 +22,25 @@ function App() {
   const handlechange = (e) => {
     setInput({ ...input, [e.target.id]: e.target.value })
   }
-  console.log(input);
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, "animals", id))
+      fetch()
+    } catch (error) {
+      console.log(alert(error));
+    }
+
+  }
+
+  const handleEdit = async (id) => {
+    try {
+      await updateDoc(doc(db, "animals", animalId), input)
+    } catch (error) {
+      console.log(alert(error));
+    }
+  }
+
 
   const handlesubmit = async (e) => {
     e.preventDefault();
@@ -39,9 +56,6 @@ function App() {
       name: "", species: "", age: ""
     })
   }
-
-
-
 
   const fetch = async () => {
     const data = await getDocs(collection(db, "animals"))
@@ -62,8 +76,8 @@ function App() {
         <Navbar />
         <div className="p-4 space-y-8">
           <Dashboard />
-          <AnimalList animals={animals} />
-          <AddAnimal handlesubmit={handlesubmit} handlechange={handlechange} input={input}/>
+          <AnimalList animals={animals} handleDelete={handleDelete} handleEdit={handleEdit} animalId={animalId} />
+          <AddAnimal handlesubmit={handlesubmit} handlechange={handlechange} input={input} />
         </div>
       </div>
     </div>
--- Firebase-fire-store/src/components/AnimalList.jsx
+++ Firebase-fire-store/src/components/AnimalList.jsx
@@ -1,4 +1,6 @@
-const AnimalList = ({animals}) => {
+const AnimalList = ({animals, handleDelete, handelEdit}) => {
+
+  console.log(handleDelete);
    return (
     <div className="p-6">
       <h2 className="text-2xl font-semibold mb-4">Animal List</h2>
@@ -8,6 +10,7 @@ const AnimalList = ({animals}) => {
             <th className="text-left py-2 px-4 border-b">Name</th>
             <th className="text-left py-2 px-4 border-b">Species</th>
             <th className="text-left py-2 px-4 border-b">Age</th>
+            <th className="text-left py-2 px-4 border-b">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -18,6 +21,10 @@ const AnimalList = ({animals}) => {
                   <td className="py-2 px-4 border-b">{animal.name}</td>
                   <td className="py-2 px-4 border-b">{animal.species}</td>
                   <td className="py-2 px-4 border-b">{animal.age}</td>
+                  <td className="py-2 px-4 border-b flex gap-5">
+                    <button className="text-yellow-500" onClick={()=>handelEdit()} >Edit</button>
+                    <button className="text-red-600" onClick={()=>handleDelete(animal.id)}>Delete</button>
+                  </td>
                 </tr>
               )
             })
